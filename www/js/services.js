d8320eeb7d99ddb58fd77ef4757b732b38796ca0
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-29 10:36:34 -0700

    put in some console logs to debug

--- www/js/services.js
+++ www/js/services.js
@@ -21,27 +21,25 @@ angular.module('starter.services', ['starter.constants', 'firebase', 'ngCordova'
   User.oauth = function(provider){
     switch(provider){
       case 'facebook':
-        console.log('Inside Facebook');
         $cordovaOauth.facebook('442668512567921', ['email']).then(function(result){
           $rootScope.afAuth.$authWithOAuthToken('facebook', result.access_token).then(function(authData){
-            showAlert('Successfully login', JSON.stringify(authData));
+            console.log('Successfully login', JSON.stringify(authData));
           }, function(error){
-            showAlert('ERROR at the firebaseAuth level', error);
+            console.log('ERROR at the firebaseAuth level', error);
           });
         }, function(error){
-          showAlert('ERROR at the facebook level',  error);
+          console.log('ERROR at the facebook level',  error);
         });
         break;
       case 'google':
-        console.log('Inside Google');
         $cordovaOauth.google('534265459229-jpvjvcbk8vmevna8i8iccrvgmb7tcp4o.apps.googleusercontent.com', ["https://www.googleapis.com/auth/urlshortener", "https://www.googleapis.com/auth/userinfo.email"]).then(function(result){
           $rootScope.afAuth.$authWithOAuthToken('google', result.access_token).then(function(authData){
-            showAlert('Successfully login', JSON.stringify(authData));
+            console.log('Successfully login', JSON.stringify(authData));
           }, function(error){
-            showAlert('ERROR at the firebaseAuth level', error);
+            console.log('ERROR at the firebaseAuth level', error);
           });
         }, function(error){
-          showAlert('ERROR at the google plus level', error);
+          console.log('ERROR at the google plus level', error);
         });
     }
   };
