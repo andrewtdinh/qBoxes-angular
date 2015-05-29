ca9bbf34c55ffa44703a0affbcb7a2c5b79a3d8c
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-29 16:41:05 -0700

    Change some minor UI items

--- www/js/constants.js
+++ www/js/constants.js
@@ -2,4 +2,5 @@
 
 angular.module('starter.constants', [])
 .constant('firebaseUrl', 'https://myfb.firebaseio.com/')
-.constant('nodeUrl', 'http://localhost:8000');
+.constant('nodeUrl', 'http://192.168.1.85:8000');
+// .constant('nodeUrl', 'http://localhost:8000');
